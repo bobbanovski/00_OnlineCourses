210e733228af5b1074320ec077a393c5e338de9d
bobbanovski <bobbanovski@gmail.com>
2021-07-10 00:04:30 +0800

    Basic unit testing in python/Pycharm

---
+++ Unit Testing and Test Driven Development in Python/pytest_test/pytest_test.py
@@ -0,0 +1,13 @@
+
+def test_AssertTrue():
+    assert True
+
+class Test_Class:
+    def test_me(self):
+        assert True
+    def test_me2(self):
+        assert True
+
+class testClass:
+    def test_it(self):
+        assert True
---
+++ Unit Testing and Test Driven Development in Python/pytest_test/test_fizzbuzz_kata.py
@@ -0,0 +1,10 @@
+import pytest
+
+# def fizzbuzz(value):
+#     return
+
+def test_AssertTrue():
+    assert True
+
+# def test_cancallfizzbuzz():
+#     assert False
